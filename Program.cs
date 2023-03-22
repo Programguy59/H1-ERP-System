d9487c6201d77283d709ccff0aef7175ff379f46
laur887b@gmail.com <laur887b@gmail.com>
2023-03-22 09:04:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace H1_ERP_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
