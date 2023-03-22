032194cece559d9f6299c9ad8ab7e5c080de1c8c
casper madsen <casp589b@elev.techcollege.dk>
2023-03-22 13:19:31 +0100

    V1 - Created CompanySetup class

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace H1_ERP_System;
+﻿using H1_ERP_System.ui;
+using TECHCOOL.UI;
+namespace H1_ERP_System;
 
 public static class Program
 {
     private static void Main(string[] args)
     {
-        
+        MyFirstScreen firstScreen = new MyFirstScreen();
+        Screen.Display(firstScreen);
     }
 }
---
+++ src/ui/CompanySetup.cs
@@ -0,0 +1,12 @@
+﻿namespace H1_ERP_System.ui;
+
+using TECHCOOL.UI;
+public class MyFirstScreen : Screen
+{
+    public override string Title { get; set; } = "My first screen";
+    protected override void Draw()
+    {
+        Clear(this);
+        Console.WriteLine("My first screen!");
+    }
+}
