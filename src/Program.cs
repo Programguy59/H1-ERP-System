d8cebf8d285427d76b35c091f7363bfdedb80c53
Bastian Asmussen <bastianasmussen@gmail.com>
2023-03-22 12:13:01 +0100

    Project file structure update.

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace H1_ERP_System;
+﻿using H1_ERP_System.util;
+
+namespace H1_ERP_System;
 
 public static class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, world!");
+        
     }
 }
