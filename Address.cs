13900c3ce0a5362db5ed53cd00e7db661b9f6c63
Bastian Asmussen <bastianasmussen@gmail.com>
2023-03-22 11:10:10 +0100

    D1 Create Company and Address classes.

---
+++ Address.cs
@@ -0,0 +1,24 @@
+﻿namespace H1_ERP_System;
+
+public class Address
+{
+	private string StreetName { get; }
+	private string StreetNumber { get; }
+	private string ZipCode { get; }
+	private string City { get; }
+	private string Country { get; }
+	
+	public Address(string streetName, string streetNumber, string zipCode, string city, string country)
+	{
+		StreetName = streetName;
+		StreetNumber = streetNumber;
+		ZipCode = zipCode;
+		City = city;
+		Country = country;
+	}
+	
+	public override string ToString()
+	{
+		return $"StreetName={StreetName}, StreetNumber={StreetNumber}, ZipCode={ZipCode}, City={City}, Country={Country}";
+	}
+}
---
+++ Company.cs
@@ -0,0 +1,36 @@
+﻿namespace H1_ERP_System;
+
+public class Company
+{
+	private int Id { get; }
+	private string CompanyName { get; }
+	
+	private Address Address { get; }
+	
+	private string Currency { get; }
+
+	public Company(int id, string companyName, Address address, string currency)
+	{
+		Id = id;
+		CompanyName = companyName;
+		
+		Address = address;
+		
+		Currency = currency;
+	}
+	
+	public Company(int id, string companyName, string streetName, string streetNumber, string zipCode, string city, string country, string currency)
+	{
+		Id = id;
+		CompanyName = companyName;
+		
+		Address = new Address(streetName, streetNumber, zipCode, city, country);
+		
+		Currency = currency;
+	}
+	
+	public override string ToString()
+	{
+		return $"Id={Id}, CompanyName={CompanyName}, Address={Address}, Currency={Currency}";
+	}
+}
