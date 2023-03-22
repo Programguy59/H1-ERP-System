b02887b408ab10a2f7f982d85bebbeff3246a05d
Bastian Asmussen <bastianasmussen@gmail.com>
2023-03-22 12:13:01 +0100

    Project file structure update.

---
+++ src/util/DatabaseServer.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace H1_ERP_System.util;
+
+public class DatabaseServer
+{
+	public static SqlConnection getConnection()
+	{
+		SqlConnectionStringBuilder sb = new()
+		{
+			DataSource = "docker.data.techcollege.dk",
+			InitialCatalog = "H1PD021123_Gruppe3",
+			UserID = "H1PD021123_Gruppe3",
+			Password = "H1PD021123_Gruppe3"
+		};
+		
+		var connectionString = sb.ToString();
+		var connection = new SqlConnection(connectionString);
+		
+		return connection;
+	}
+}
