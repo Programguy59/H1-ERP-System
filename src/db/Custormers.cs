e9285d08255e151a2057b26f6e4d87ff073a9b65
laur887b@gmail.com <laur887b@gmail.com>
2023-03-24 10:54:52 +0100

    K1 - Added to Database class with Customer functionality

---
+++ src/db/Custormers.cs
@@ -0,0 +1,62 @@
+﻿using H1_ERP_System.customer;
+using H1_ERP_System.src.customer;
+
+namespace H1_ERP_System.db;
+
+public partial class Database
+{
+    private static readonly List<Customer> Customers = new();
+    private static int _nextCustomerId = 1;
+
+    public static Customer GetCustomerById(int id)
+    {
+        return Customers.FirstOrDefault(customer => customer.Id == id)!;
+    }
+
+    public static List<Customer> GetAllCustomers()
+    {   
+        return Customers;
+    }
+
+    public static void InsertCustomer(Customer customer)
+    {
+        customer.Id = _nextCustomerId++;
+
+        Customers.Add(customer);
+    }
+
+    public static bool UpdateCustomer(Customer customer, int id)
+    {
+        var existingCustomer = GetCustomerById(id);
+
+        if (existingCustomer == null)
+        {
+            return false;
+        }
+
+        existingCustomer.PersonFullName = customer.PersonFullName;
+        existingCustomer.Address = customer.Address;
+        existingCustomer.Email = customer.Email;
+
+        return true;
+    }
+
+    public static bool DeleteCustomerById(int id)
+    {
+        var customerToDelete = GetCustomerById(id);
+
+        if (customerToDelete == null)
+        {
+            return false;
+        }
+
+        Customers.Remove(customerToDelete);
+
+        return true;
+    }
+
+    public static void ClearCustomers()
+    {
+        Customers.Clear();
+    }
+}
