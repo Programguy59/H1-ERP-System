75bb3f48e05f1b9c69ba8f9d23f99fde27603668
laur887b@gmail.com <laur887b@gmail.com>
2023-03-28 09:30:02 +0200

    P2 - Added list of Products screen

---
+++ src/ui/Products/ProductSetupList.cs
@@ -0,0 +1,33 @@
+﻿using H1_ERP_System.products;
+
+namespace H1_ERP_System.ui;
+
+    
+public class ProductSetupList
+    {
+        public enum ProductSetupState { Todo, Started, Done }
+
+    public int ProductNumber { get; set; }
+    public string ProductName { get; set; } = "";
+    public double SalesPrice { get; set; }
+    public double PurchasePrice { get; set; }
+    public double ProfitMargin { get; set; }
+    public double Stock { get; set; }
+    public int Priority { get; set; }
+    public ProductSetupState State {get;set;}
+    
+        public ProductSetupList(Product product, int priority=1)
+        {
+            ProductNumber = product.Id; 
+            ProductName = product.Name; 
+            SalesPrice = product.SalesPrice; 
+            PurchasePrice = product.PurchasePrice; 
+            ProfitMargin = product.ProfitMargin; 
+            Stock = product.Stock; 
+            Priority = priority;
+            
+
+            
+            Priority = priority;
+        }
+}
