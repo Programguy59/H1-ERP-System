13881132c34647940be6be7a95f0ed3c5729c6b1
casper madsen <casp589b@elev.techcollege.dk>
2023-03-28 08:41:24 +0200

    S1 - S2 "created a screen for sales Data"

---
+++ src/ui/sale/SalesList.cs
@@ -0,0 +1,23 @@
+﻿namespace H1_ERP_System.ui.sale;
+
+public class SalesList
+{
+    public int Id { get; }
+    
+    public string Date { get; }
+    public string CustomerId { get; }
+    public string CustomerName { get; }
+    
+    public double TotalPrice { get; }
+    
+    public SalesList(int id, string date, string customerId, string customerName, double totalPrice)
+    {
+        Id = id;
+        
+        Date = date;
+        CustomerId = customerId;
+        CustomerName = customerName;
+        
+        TotalPrice = totalPrice;
+    }
+}
