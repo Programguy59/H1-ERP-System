4b01b072f868d9553600a0ccbcfcfeb40b0cf4b3
Bastian Asmussen <bastianasmussen@gmail.com>
2023-03-29 14:45:50 +0200

    U1 - Updated tables to implement external addresses table, and updated classes to use it.

--- src/ui/Products/ProductSetupScreen.cs
+++ src/ui/Products/ProductSetupScreen.cs
@@ -4,31 +4,20 @@ using H1_ERP_System.util;
 namespace H1_ERP_System.ui;
 
 using H1_ERP_System.products;
-using H1_ERP_System.src.ui.Company;
 using TECHCOOL.UI;
 public class ProductSetupScreen : Screen
 {
     public override string Title { get; set; } = "product setup";
-
     public Product product = new Product(88, "CoolProduct", "It is cool", 25, 30, "Bag dig", 3, Unit.Meters);
     public Product product2 = new Product(44, "unCoolProduct", "It is  not cool", 35, 30, "foran dig", -3, Unit.Meters);
 
-
-
     protected override void Draw()
     {
-
-
-
         Clear(this);
         ListPage<ProductScreenList> listPage = new ListPage<ProductScreenList>();
-        var Products = DatabaseServer.FetchProducts();
-        for (int i = 0; i < Products.Count; i++)
-        {
-            listPage.Add(new ProductScreenList(Products[i], 1));
-        }
-        
-
+        listPage.Add(new ProductScreenList(product, 1));
+        listPage.Add(new ProductScreenList(product2, 1));
+    
         listPage.AddColumn("Product number", "ProductNumber" );
         listPage.AddColumn("Product", "ProductName" );
         listPage.AddColumn("Sales Price", "SalesPrice");
@@ -46,7 +35,4 @@ public class ProductSetupScreen : Screen
                 
                 Screen.Display(new ProductScreen(selected));
     }
-
-
-
 }
