81b239eac4e13f0f45cbaa730bb035d8c0c6aab4
casper madsen <casp589b@elev.techcollege.dk>
2023-04-14 13:25:47 +0200

    S3 - Added unity test for testing order

---
+++ ErpTests/UnitTests/SalesUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using H1_ERP_System.customer;
+using H1_ERP_System.sales;
+
+namespace ErpTests.UnitTests;
+
+public class Tests
+{
+    
+    [Fact]
+    public void TestOrder()
+    {
+        var order = new Order(1, "2021-01-0", "2", null, OrderStatus.Completed);
+        Assert.NotNull(order);
+    }
+}
