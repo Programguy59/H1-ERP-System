f80ed21c4f7c5ee23c7a4ff207cb7ef0300b03b0
laur887b@gmail.com <laur887b@gmail.com>
2023-04-14 10:57:30 +0200

    U1 - Major Refactoring to enable Unit Testing

---
+++ ErpTests/UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using H1_ERP_System.products;
+
+namespace ErpTests.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
