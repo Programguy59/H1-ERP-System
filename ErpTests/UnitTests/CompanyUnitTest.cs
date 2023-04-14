b10dbadff0853b903318703948498a2d47ff92a2
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2023-04-14 13:38:08 +0200

    D1 - Oprettede unit test for ToString for company

---
+++ ErpTests/UnitTests/CompanyUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using H1_ERP_System.util;
+using System;
+using System.Collections.Generic;
+using H1_ERP_System.company;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpTests.UnitTests
+{
+
+    public class CompanyUnitTest
+    {
+        
+        [Fact]
+        public void ToStringTest()
+        {
+            Address SuperAddress = new Address(0, "", "", "", "", "");
+            Company company = new(0, "BestCompany", SuperAddress, "USD");
+            Assert.IsType<string>(company.ToString());
+        }  
+    }
+}
