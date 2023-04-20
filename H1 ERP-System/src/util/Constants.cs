ae85469db2cdf43d0368ffd8d284255274aa7217
Bastian Asmussen <bastianasmussen@gmail.com>
2023-04-20 09:44:12 +0200

    U1 - Fixed CustomerId and PersonId conflicts/overlaps. Added DB connection data to Constants.cs.

--- H1 ERP-System/src/util/Constants.cs
+++ H1 ERP-System/src/util/Constants.cs
@@ -3,4 +3,14 @@
 public static class Constants
 {
 	public const int DefaultId = -1;
+
+	public static class Sql
+	{
+		public const string Host = "192.168.1.70";
+		public const int Port = 1433;
+
+		public const string Database = "H1PD021123_Gruppe3";
+		public const string User = "H1PD021123_Gruppe3";
+		public const string Password = "H1PD021123_Gruppe3";
+	}
 }
